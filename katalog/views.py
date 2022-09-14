bc0cf93da6d0fc0a6dca463d1fd54c5c5a2d5b24
Rathina Shabur <rathinashabur@MacBook-Pro-Sugeng.local>
2022-09-14 15:28:33 +0700

    Tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_item_catalog = CatalogItem.objects.all()
+    context = {
+        'item_katalog': data_item_catalog,
+    }
+    return render(request, "katalog.html", context)
--- project_django/settings.py
+++ project_django/settings.py
@@ -134,8 +134,8 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-for directory in [*STATICFILES_DIRS, STATIC_ROOT]:
-    directory.mkdir(exist_ok=True)
+# for directory in [*STATICFILES_DIRS, STATIC_ROOT]:
+#    directory.mkdir(exist_ok=True)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
